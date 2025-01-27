3e654044570756261fe32ab8261ef3c88a36be9d
owanedee <claudiojorge9090@gmail.com>
2025-01-27 09:32:59 +0100

    Adjusting efficiency and coherence

--- Cell.lua
+++ Cell.lua
@@ -1,29 +1,21 @@
 local love = require("love")
 
-function Cell() 
-    local x
-    local y
-    local size
+function Cell(x, y, size)
+    return {
+        x = x,
+        y = y,
+        size = size,
 
-return{
+        draw = function(self)
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.rectangle("fill", self.x, self.y, self.size, self.size)
+        end,
 
-    draw = function (self,x,y,side_Size) -- draw a cell given x and y coordinates and a size
-        self.x = x
-        self.y = y
-        self.size = side_Size
-        love.graphics.setColor(0, 0, 0)
-        love.graphics.rectangle("fill",x,y,side_Size,side_Size)
-    end,
-
-    checkPressed = function (self, x_Clicked, y_Clicked) -- checks if the player clicked on this cell
-        return (x_Clicked >= self.x and x_Clicked <= self.x + self.size 
-                and y_Clicked >= self.y and y_Clicked <= self.y + self.size)
-         
-    end
-}
-
-
+        checkPressed = function(self, x_Clicked, y_Clicked)
+            return (x_Clicked >= self.x and x_Clicked <= self.x + self.size 
+                    and y_Clicked >= self.y and y_Clicked <= self.y + self.size)
+        end
+    }
 end
-
 
 return Cell
--- main.lua
+++ main.lua
@@ -3,9 +3,7 @@ local Dice = require("Dice")
 local Cell = require("Cell")
 
 local dice = Dice(6)
-local window_Width = love.graphics.getWidth()
-local window_Height = love.graphics.getHeight()
-local matrix_Rect_Size = 100
+local cells = {}
 
 local player_Matrix = {
     {0,0,0},
@@ -19,24 +17,29 @@ local enemy_Matrix = {
     {0,0,0}
 }
 
-local Cell = Cell()
-
-local function drawPlayerMatrix() 
-    local offset = matrix_Rect_Size / 3
-    for i = -1, 1 do
-        Cell:draw(window_Width / 2.25 + i*( matrix_Rect_Size + offset), 
-                  window_Height - (4*matrix_Rect_Size), matrix_Rect_Size)
-        Cell:draw(window_Width / 2.25 + i*( matrix_Rect_Size + offset), 
-                  window_Height - (3*matrix_Rect_Size) + offset, matrix_Rect_Size)
-        Cell:draw(window_Width / 2.25 + i*( matrix_Rect_Size + offset), 
-                  window_Height - (1.7*matrix_Rect_Size) + offset, matrix_Rect_Size)
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 then
+        for _,cell in pairs(cells) do
+            if cell:checkPressed(x, y) then
+                -- logic for when a cell is Clicked
+                love.graphics.setBackgroundColor(0,0,0)
+            end
+        end
     end
-
-    
 end
 
 function love.load()
     love.graphics.setBackgroundColor(115 / 255, 160 / 255, 30 / 255) -- green
+
+    local cellSize = 100
+    local offset = cellSize / 3
+    for i = 0, 2 do
+        for j = 0, 2 do
+            local x = 200 + j * (cellSize + offset)
+            local y = 200 + i * (cellSize + offset)
+            table.insert(cells, Cell(x, y, cellSize))
+        end
+    end
 end
 
 function love.update(dt)
@@ -44,19 +47,14 @@ function love.update(dt)
 end
 
 function love.draw()
+    
     love.graphics.setColor(0,0,0) -- black
     love.graphics.rectangle("fill",30,30,100,100)
     love.graphics.setColor(0.5,0.5,0.5) -- grey
     love.graphics.rectangle("fill",35,35,90,90)
     -- dice:draw(55,55,50,50)
 
-    for i = 1, #player_Matrix do
-        for j = 1, #player_Matrix[i] do
-            drawPlayerMatrix()
-
-            if player_Matrix[i][j] ~= 0 then
-                
-            end
-        end
+    for _, cell in pairs(cells) do
+        cell:draw()
     end
 end
