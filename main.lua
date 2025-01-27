a4e5905003fcfe37382161a85d839132e338ffb1
owanedee <claudiojorge9090@gmail.com>
2025-01-27 09:44:02 +0100

    Created Enemy cells

--- main.lua
+++ main.lua
@@ -3,7 +3,11 @@ local Dice = require("Dice")
 local Cell = require("Cell")
 
 local dice = Dice(6)
-local cells = {}
+local playerCells = {}
+local enemyCells = {}
+
+local window_Width = love.graphics.getWidth()
+local window_Height = love.graphics.getHeight()
 
 local player_Matrix = {
     {0,0,0},
@@ -19,7 +23,7 @@ local enemy_Matrix = {
 
 function love.mousepressed(x, y, button, istouch, presses)
     if button == 1 then
-        for _,cell in pairs(cells) do
+        for _,cell in pairs(playerCells) do
             if cell:checkPressed(x, y) then
                 -- logic for when a cell is Clicked
                 love.graphics.setBackgroundColor(0,0,0)
@@ -31,13 +35,20 @@ end
 function love.load()
     love.graphics.setBackgroundColor(115 / 255, 160 / 255, 30 / 255) -- green
 
-    local cellSize = 100
-    local offset = cellSize / 3
+    local cell_Size = 100
+    local offset = cell_Size / 4
     for i = 0, 2 do
         for j = 0, 2 do
-            local x = 200 + j * (cellSize + offset)
-            local y = 200 + i * (cellSize + offset)
-            table.insert(cells, Cell(x, y, cellSize))
+            local x = window_Width / 2.7 + j * (cell_Size + offset)
+            local y = window_Height / 1.7 + i * (cell_Size + offset)
+            table.insert(playerCells, Cell(x, y, cell_Size))
+        end
+    end
+    for i = 0, 2 do
+        for j = 0, 2 do
+            local x = window_Width / 2.7 + j * (cell_Size + offset)
+            local y = 10 + i * (cell_Size + offset)
+            table.insert(enemyCells, Cell(x,y,cell_Size))
         end
     end
 end
@@ -54,7 +65,14 @@ function love.draw()
     love.graphics.rectangle("fill",35,35,90,90)
     -- dice:draw(55,55,50,50)
 
-    for _, cell in pairs(cells) do
+    -- draw the player cells
+    for _, cell in pairs(playerCells) do
         cell:draw()
     end
+
+    -- draw the enemy cells
+    for _, cell in pairs(enemyCells) do
+        cell:draw()
+    end
+
 end
